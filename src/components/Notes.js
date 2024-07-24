47a99358289fa192f5e45535e9d8677f25719d05
Vijaya Kumar Annam <118079473+Vijaykumar069@users.noreply.github.com>
2024-07-24 20:54:21 +0530

    Update Notes.js

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,9 +1,50 @@
+// import React, { useContext, useEffect } from 'react';
+// import { NoteContext } from '../context/notes/NoteContext';
+// import NoteItem from './NoteItem';
+// import empty from '../images/empty.png';
+// import empty1 from '../images/empty1.png';
+// import empty3 from '../images/empty3.jpeg';
+// import { useNavigate } from "react-router-dom";
+// import { AlertContext } from '../context/AlertContext';
+
+// function Notes() {
+//     const { notes, getNotes } = useContext(NoteContext);
+//     const navigate = useNavigate();
+//     const { showAlert } = useContext(AlertContext);
+
+//     useEffect(() => {
+//         if (localStorage.getItem('token')) {
+//             getNotes();
+//         } else {
+//             navigate('/about');
+//             showAlert("You need to sign in first", "error");
+//         }
+//         // eslint-disable-next-line
+//     }, []);
+
+//     return (
+//         <div className="row ps-5 mt-4 mb-1">
+//             <h1 className="display-6">Your Notes: </h1>
+//             {(!Array.isArray(notes) || notes.length === 0) && 
+//                 <div className="d-flex">
+//                     <p style={{ position: "absolute", left: "35%", bottom: "-10%", top:"30%" }}>Create your first note :) !!!</p>
+//                     <img className="img-fluid ms-5 mt-3" src={empty1} alt="empty" style={{ width: "75%", opacity: "1.0" }} />
+//                 </div>
+//             }
+//             {Array.isArray(notes) && notes.map(note => 
+//                 <NoteItem key={note._id} note={note} />
+//             )}
+//         </div>
+//     );
+// }
+
+// export default Notes;
+
+
 import React, { useContext, useEffect } from 'react';
 import { NoteContext } from '../context/notes/NoteContext';
 import NoteItem from './NoteItem';
-import empty from '../images/empty.png';
 import empty1 from '../images/empty1.png';
-import empty3 from '../images/empty3.jpeg';
 import { useNavigate } from "react-router-dom";
 import { AlertContext } from '../context/AlertContext';
 
@@ -23,20 +64,22 @@ function Notes() {
     }, []);
 
     return (
-        <div className="row ps-5 mt-4 mb-1">
+        <div className="container mt-4 mb-1">
             <h1 className="display-6">Your Notes: </h1>
-            {(!Array.isArray(notes) || notes.length === 0) && 
-                <div className="d-flex">
-                    <p style={{ position: "absolute", left: "35%", bottom: "-10%", top:"30%" }}>Create your first note :) !!!</p>
-                    <img className="img-fluid ms-5 mt-3" src={empty1} alt="empty" style={{ width: "75%", opacity: "1.0" }} />
+            {Array.isArray(notes) && notes.length > 0 ? (
+                <div className="row ps-5">
+                    {notes.map(note => 
+                        <NoteItem key={note._id} note={note} />
+                    )}
                 </div>
-            }
-            {Array.isArray(notes) && notes.map(note => 
-                <NoteItem key={note._id} note={note} />
+            ) : (
+                <div className="text-center mt-4">
+                    <p>Create your first note :) !!!</p>
+                    <img className="img-fluid mt-3" src={empty1} alt="empty" style={{ maxWidth: "90%", opacity: "1.0" }} />
+                </div>
             )}
         </div>
     );
 }
 
 export default Notes;
-
